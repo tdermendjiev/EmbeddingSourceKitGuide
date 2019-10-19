c3b48f2c09e6aa9753804330cc98f8cb34ea2925
tdermendjiev <tdermendjievft@gmail.com>
2019-10-19 19:55:44 +0300

    Initial Commit

---
+++ EmbeddingSourceKitGuide/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EmbeddingSourceKitGuide
+//
+//  Created by Teodor Dermendzhiev on 19.10.19.
+//  Copyright © 2019 Teodor Dermendzhiev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
